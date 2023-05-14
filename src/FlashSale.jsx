8f2ff8df527993f4dd919e7fae1a8a5396a79a10
Balkrishna pokharel <58511308+krishbale@users.noreply.github.com>
2023-05-14 15:21:10 +0000

    flashsale added

--- src/FlashSale.jsx
+++ src/FlashSale.jsx
@@ -3,22 +3,25 @@ import './flashsale.css'
 import Card from './Card'
 
 const FlashSale = () => {
-  let arr=[1,2,3,4,5,6,7,8,9,]
+  let arr=[1,2,3,4,5,6,7,8,]
   return (
-    <div className='darazclone__flashsale section__margin'>
-      {/* <p>On Sale Now 
+    <div className='darazclone__flashsale'>
+     <div className='darazclone__flashsale-text'>
+     <p>On Sale Now 
       </p>
       <p>Ending IN</p>
+     </div>
     <div className='darazclone__flashsale-countdown'>
+   
     <p>00:</p>
     <p>00:</p>
     <p>00:</p>
-    <p>00:</p>
-    </div> */}
+    <p>00</p>
+    </div> 
     <div className='darazclone__flashsale-product'>
-       {/* {arr.map((item,index) => 
+       {arr.map((item,index) => 
           <Card  item = {item} key={index} />
-         )} */}
+         )}
     </div>
 
     </div>
